3b2bdc4045a51e3b11eea4e3893507bbd951ee56
Hampus Londögård <hampus.londogard@gmail.com>
2022-10-17 10:25:26 +0200

    Add DF with fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,19 @@
 import streamlit as st
+import pandas as pd
 
-st.title("My Parents New Healthy Diner")
+def main():
+    st.title("My Parents New Healthy Diner")
 
-st.header("Breakfast Favorites")
-st.text("🥣 Omega 3 & Blueberry Oatmeal")
-st.text("🥗 Kale, Spinach & Rocket Smoothie")
-st.text("🐔 Hard-Boiled Free-Range Egg")
-st.text("🥑🍞 Avocado Toast")
+    st.header("Breakfast Favorites")
+    st.text("🥣 Omega 3 & Blueberry Oatmeal")
+    st.text("🥗 Kale, Spinach & Rocket Smoothie")
+    st.text("🐔 Hard-Boiled Free-Range Egg")
+    st.text("🥑🍞 Avocado Toast")
 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+    st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+    my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    st.dataframe(my_fruit_list)
+
+if __name__ == '__main__':
+    main()
