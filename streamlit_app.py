9ea3f0e1cbacc85ec19c764d2b5c7e628e475a8c
Hampus Londögård <hampus.londogard@gmail.com>
2022-10-17 10:26:38 +0200

    Add multi-select

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ def main():
     st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
     my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
     st.dataframe(my_fruit_list)
 
 if __name__ == '__main__':
