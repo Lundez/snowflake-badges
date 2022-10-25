526b7720947ae252771e1979de316a2ded04c4e4
Hampus Londögård <hampus.londogard@gmail.com>
2022-10-25 08:06:30 +0200

    feat: Let user choose fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,11 @@ def main():
     
     st.dataframe(fruits_to_show)
 
+    fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+    st.write('The user entered ', fruit_choice)
+
     st.header("Fruityvice Fruit Advice!")
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
     st.dataframe(fruityvice_normalized)
