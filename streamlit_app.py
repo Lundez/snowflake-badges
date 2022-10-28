a7d398270b974def64a32a0f24164ed05724566d
Hampus Londögård <hampus.londogard@gmail.com>
2022-10-28 15:18:53 +0200

    fruit list query

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,9 @@ def main():
 
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     my_cur = my_cnx.cursor()
-    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
     my_data_row = my_cur.fetchone()
-    st.text("Hello from Snowflake:")
+    st.text("The fruit list contains: ")
     st.text(my_data_row)
 
 if __name__ == '__main__':
