e9628b55cbb8b7bd4eaf56e784b8bb7bb78244e8
Hampus Londögård <hampus.londogard@gmail.com>
2022-10-28 15:17:56 +0200

    test snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,5 +31,12 @@ def main():
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
     st.dataframe(fruityvice_normalized)
 
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_data_row = my_cur.fetchone()
+    st.text("Hello from Snowflake:")
+    st.text(my_data_row)
+
 if __name__ == '__main__':
     main()
