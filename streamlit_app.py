6a61bf511fd6e4ed004d14c4137c31ab57297a39
Hampus Londögård <hampus.londogard@gmail.com>
2022-10-28 15:31:59 +0200

    if-else added

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,17 @@ def main():
     
     st.dataframe(fruits_to_show)
 
-    fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-    st.write('The user entered ', fruit_choice)
-
     st.header("Fruityvice Fruit Advice!")
-    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    st.dataframe(fruityvice_normalized)
+    try:
+        fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+        if not fruit_choice:
+            st.error("Please select a fruit")
+        else:
+            fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+            fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+            st.dataframe(fruityvice_normalized)
+    except URLError as e:
+        st.error()
 
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     my_cur = my_cnx.cursor()
